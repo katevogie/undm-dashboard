cef61f3a2c5c3e5569da456024b7e5f8fdc9c9ea
katevogie <kaitlynnvogel@gmail.com>
2022-03-27 11:58:05 -0500

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -63,7 +63,7 @@ class Dashboard extends React.Component{
             <div className="countdown">
                 <span>🚀</span>
                 <Countdown date={new Date("Feb 26, 2022 23:45:00")} daysInHours={true}/>
-                <span style={{padding: 0}}> until Huskerthon blastoff!</span>
+                <span style={{padding: 0}}> until dm blastoff!</span>
                 <span>🚀</span>
             </div>
         </Row>
